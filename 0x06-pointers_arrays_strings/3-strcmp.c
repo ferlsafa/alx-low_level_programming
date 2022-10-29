35ee2c44b99e53ee91f879e7c6f8db2150eaeef6
ferlsafa <djrfredy@gmail.com>
2022-10-29 12:23:19 -0700

    the rest

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,20 @@
-#include <stdio.h>  
-#include<string.h>  
-int main()  
-{  
-   char str1[20];  // declaration of char array  
-   char str2[20];  // declaration of char array  
-   int value; // declaration of integer variable  
-   printf("Enter the first string : ");  
-   scanf("%s",str1);  
-   printf("Enter the second string : ");  
-   scanf("%s",str2);  
-   // comparing both the strings using strcmp() function  
-   value=strcmp(str1,str2);  
-   if(value==0)  
-   printf("strings are same");  
-   else  
-   printf("strings are not same");  
-   return 0;  
-} 
+include "main.h"
+
+/**
+ * _strcmp - A function that compares two strings
+ * @s1: string to compare
+ * @s2: string to compare
+ * Return: 0 and difference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+{
+	if (s1[i] != s2[i])
+	return (s1[i] - s2[i]);
+}
+return (0);
+}
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * string_toupper - a function that changes all the lower letter
+ * of a string to uppercase
+ * @n:The string
+ * Return: capitalized letter
  */
-int main()
-{
-    char str[] = "Look up!!\n";
-    char *ptr;
 
-    ptr = string_toupper(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] -= 32;
+	}
+	return (n);
 }
